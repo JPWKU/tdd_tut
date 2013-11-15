2e704f7dcfb2abf16ac56ceb6dc42e9e6dc7a745
Jeremy Phelps <jeremy.phelps769@topper.wku.edu>
2013-11-15 11:20:46 -0600

    removed duplication of checking for row in table

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = self.browser.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Jay heard about a cool new to-do list app online
         #so he goes to check it out
@@ -42,17 +47,14 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('1: Buy peacock feathers',[row.text for row in rows])
 
         #there is still a text box to add another item
-        #he enters "Buy peacock feathers to make a fly"
+        #he enters "Use peacock feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
-        inputbox.send_keys('Buy peacock feathers to make a fly')
+        inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
 
         #the page updates again and now shows both items in to-do list
-        table = self.browser.find_element_by_id('id_list_table')
-        row = self.browser.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Buy peacock feathers to make a fly',
-                      [row.text for row in rows])
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         #he wonders if the site will remember his list and notices
         #that the site has generated a unique url for him 
